552333d71fba238cdb64817113c6077a88c6984b
edel-ops <ebonilla6@gmail.com>
2021-09-21 14:12:01 -0600

    modificando rutas & agregando folder assets

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Controllers\Web;
+namespace App\Controllers;
 
-use App\Controllers\BaseController;
+// use App\Controllers\BaseController;
 
 class Login extends BaseController
 {
