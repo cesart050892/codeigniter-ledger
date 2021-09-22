ef44629c9f84569fc535e02e6b9685b013a61226
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-09-22 07:09:39 -0600

    creando la entidad de usuario

--- app/Models/Users.php
+++ app/Models/Users.php
@@ -11,7 +11,7 @@ class Users extends Model
     protected $primaryKey           = 'id';
     protected $useAutoIncrement     = true;
     protected $insertID             = 0;
-    protected $returnType           = 'array';
+    protected $returnType           = \App\Entities\Users::class;
     protected $useSoftDeletes       = true;
     protected $protectFields        = true;
     protected $allowedFields        = ['name', 'username', 'surname', 'email', 'password'];
