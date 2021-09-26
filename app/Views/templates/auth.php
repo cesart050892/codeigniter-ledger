be05a9ca4af7a30c877b04e69b3268a2d0f65143
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-09-26 07:42:57 -0600

    corrigiendo template auth

--- app/Views/templates/auth.php
+++ app/Views/templates/auth.php
@@ -12,7 +12,7 @@
   <title>Ledger-Login</title>
 
   <!-- Custom fonts for this template-->
-  <link href="assets/plugins/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="<?= base_url('assets/plugins/fontawesome-free/css/all.min.css') ?>" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
   <!-- Custom styles for this template-->
   <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
@@ -27,14 +27,14 @@
   <?= $this->renderSection('content') ?>
 
   <!-- Bootstrap core JavaScript-->
-  <script src="assets/plugins/jquery/jquery.min.js"></script>
-  <script src="assets/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="<?= base_url('assets/plugins/jquery/jquery.min.js') ?>"></script>
+  <script src="<?= base_url('assets/plugins/bootstrap/js/bootstrap.bundle.min.js') ?>"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="assets/plugins/jquery-easing/jquery.easing.min.js"></script>
+  <script src="<?= base_url('assets/plugins/jquery-easing/jquery.easing.min.js') ?>"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="assets/js/sb-admin-2.min.js"></script>
+  <script src="<?= base_url('assets/js/sb-admin-2.min.js') ?>"></script>
   <!-- SweetAlert2 -->
   <script src="<?= base_url('assets/plugins/sweetalert2/sweetalert2.min.js') ?>"></script>
 
