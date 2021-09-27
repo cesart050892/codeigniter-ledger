a4b4e8238ac64f8a54afd2f31ecfcecdd8608602
Cesar Augusto Tapia <cesart050892@users.noreply.github.com>
2021-09-26 22:57:01 -0600

    corrigiendo crear cuentas

--- app/Controllers/Api/Accounts.php
+++ app/Controllers/Api/Accounts.php
@@ -68,7 +68,7 @@ class Accounts extends ResourceController
             ))) {
                 $data = [
                     'code'      =>  $this->request->getPost('input-code'),
-                    'type_fk'   =>  $this->request->getPost('input-type'),
+                    'nature_fk'   =>  $this->request->getPost('input-type'),
                     'account'   =>  $this->request->getPost('input-account')
                 ];
                 $account = new \App\Entities\Accounts($data);
